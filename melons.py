05a2b82f37275152638d73747e4d6e350e70bda2
Hackbright Student <info@hackbrightacademy.com>
2016-07-14 17:30:59 -0700

    Completed parts 2-4

--- melons.py
+++ melons.py
@@ -1,22 +1,35 @@
 """This file should have our order classes in it."""
+from random import randint
 
 class AbstractMelonOrder(object):
     """ A generic melon order """
 
-    def __init__(self, species, qty):
-        """Initialize melon order attributes"""
+    def __init__(self, species, qty, order_type, tax=0.09):
+        """Initialize melon order attributes."""
 
         self.species = species
         self.qty = qty
         self.shipped = False
-        self.tax = 0.09
+        self.tax = tax
+        self.order_type = order_type
+
+    
+    def get_base_price(self):
+        """Return a random base price between 5-9"""
+
+        random_base_price = randint(5,9)
+        return random_base_price
 
 
     def get_total(self):
         """Calculate price."""
 
-        base_price = 5
+        base_price = self.get_base_price()
+
+        if self.species == "Christmas melon":
+            base_price *= 1.5
         total = (1 + self.tax) * self.qty * base_price
+
         return total
 
 
@@ -32,21 +45,7 @@ class DomesticMelonOrder(AbstractMelonOrder):
     def __init__(self, species, qty):
         """Initialize melon order attributes"""
         
-        self.species = species
-        self.qty = qty
-        self.order_type = "domestic"
-        self.tax = 0.08
-
-
-    def get_total(self):
-        """Calculate price."""
-
-        base_price = 5
-        if self.species == "Christmas melon":
-            base_price *= 1.5
-        total = (1 + self.tax) * self.qty * base_price
-        return total
-
+        super(DomesticMelonOrder, self).__init__(species, qty, "domestic", 0.08)
 
 
 class InternationalMelonOrder(AbstractMelonOrder):
@@ -55,21 +54,14 @@ class InternationalMelonOrder(AbstractMelonOrder):
     def __init__(self, species, qty, country_code):
         """Initialize melon order attributes"""
         
-        self.species = species
-        self.qty = qty
+        super(InternationalMelonOrder, self).__init__(species, qty, "international", 0.17)
         self.country_code = country_code
-        self.order_type = "international"
-        self.tax = 0.17
 
 
     def get_total(self):
         """Calculate price."""
 
-        base_price = 5
-        if self.species == "Christmas melon":
-            base_price *= 1.5
-            
-        total = (1 + self.tax) * self.qty * base_price
+        total = super(InternationalMelonOrder, self).get_total()
 
         if self.qty < 10:
             total += 3
@@ -81,3 +73,27 @@ class InternationalMelonOrder(AbstractMelonOrder):
         """Return the country code."""
 
         return self.country_code
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+    """Governmental melon order."""
+
+    def __init__(self, species, qty):
+        """Initialize melon order attributes"""
+  
+        self.species = species
+        self.qty = qty
+        self.tax = 0
+
+   
+
+    def mark_inspection(self, passed):
+        """ Update passed_inspection to true if inspection passed """
+
+        passed_inspection = False
+
+        if passed is True:
+            passed_inspection = True
+            print True
+        
+
+
